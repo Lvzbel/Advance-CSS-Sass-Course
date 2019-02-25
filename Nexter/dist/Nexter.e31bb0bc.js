4a8f3c231aa116c9e9bebfc01c530063d047c9af
Rodrigo <rcoto@protonmail.com>
2019-02-24 22:52:01 -0800

    Section 9, Lecture 121

--- Nexter/dist/Nexter.e31bb0bc.js
+++ Nexter/dist/Nexter.e31bb0bc.js
@@ -176,7 +176,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/back.jpg":[["back.9cd3ab62.jpg","img/back.jpg"],"img/back.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./../img/hero.jpeg":[["hero.ac0612d9.jpeg","img/hero.jpeg"],"img/hero.jpeg"],"./../img/back.jpg":[["back.9cd3ab62.jpg","img/back.jpg"],"img/back.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./sass/main.scss");
@@ -207,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40589" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33421" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
